50a3de61cb2645ce53f005791a1c929057d18408
Scott Keplinger <skeplin17@gmail.com>
2020-01-24 20:37:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {"Phil Ochs": "Here's to the State of Mississippi"};
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  playlist["The Rolling Stones"] = "Paint It Black"
+  playlist["Slowdive"] = "Alsion"
    return playlist;
  }
