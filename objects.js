dbefc4dc54201441e809be39ff10c32a35571e71
Scott Keplinger <skeplin17@gmail.com>
2020-01-24 20:35:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,5 @@ var playlist = {"The Beatles": "Hey Jude"};
 
 function updatePlaylist (playlist, artistName, songTitle) {
   playlist["The Rolling Stones] = "Paint It Black"
+   return playlist;
+ }
